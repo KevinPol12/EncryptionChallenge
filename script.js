a83438e355350698a171e5226e0692289a5305bf
Kevin Polanco <kevinpolanco12@gmail.com>
2023-01-24 00:24:58 -0500

    Used the querySelector and .addEventListener to associate the click action to the Encrypt botton and process and encryption using the switch and for loop to replacement of encryption

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
 /* Regras Codificador: 
+"a" é convertido para "ai"
 "e" é convertido para "enter" 
 "i" é convertido para "imes"
-"a" é convertido para "ai"
 "o" é convertido para "ober"
 "u" é convertido para "ufat"
 Apenas letras minúsculas
@@ -10,11 +10,69 @@ Não permite acentuação
 */
 
 /* Regras Decodificador: 
+"ai" é convertido para "a"
 "enter" é convertido para "e" 
 "imes" é convertido para "i"
-"ai" é convertido para "a"
 "ober" é convertido para "o"
 "ufat" é convertido para "u"
 Apenas letras minúsculas
 Não permite acentuação     
 */
+
+
+var texto = document.querySelector("#input-texto");
+
+var msg = document.querySelector("#msg");
+
+let encriptado = "";
+
+
+document.querySelector("#btn-crypt").addEventListener("click", function(){
+
+    let textE = texto.value.toLowerCase();
+    encriptado ="";
+    msg.value = "";
+
+    for (i = 0; i < textE.length; i++){
+        console.log("length is " +textE.length);
+
+        switch(textE[i]){
+            case "a": 
+                encriptado += "ai";
+                break;
+
+            case "e": 
+                encriptado += "enter";
+                break;
+
+            case "i": 
+                encriptado += "imes";
+                break;
+
+            case "o": 
+                encriptado += "ober";
+                break;
+
+            case "u": 
+                encriptado += "ufat";
+                break;
+            default:
+                encriptado += textE[i];
+                                
+            } //end switch
+
+
+        } //end for
+
+                
+        console.log(encriptado);
+
+        msg.value = encriptado;
+
+        texto.value="";
+        texto.focus();
+
+});
+
+
+
